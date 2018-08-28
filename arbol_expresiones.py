3ef9b74019925f048bf3efd95188f6d055023095
ddguerra90 <31446845+ddguerra90@users.noreply.github.com>
2018-08-28 18:56:51 -0500

    Add files via upload

--- arbol_expresiones.py
+++ arbol_expresiones.py
@@ -1,9 +1,16 @@
 from pila import *
 from arbol import *
 
+dictionary = {
+    }
+
+def updateDict(let, val):    
+    dictionary[let.valor] = val
+    print let.valor+' = '+str(dictionary[let.valor])
+
 def convertir(lista, pila):
     if lista != []:
-        if lista[0] in "+-*/":
+        if lista[0] in "+-*/=":           
             nodo_der = pila.desapilar()
             nodo_izq = pila.desapilar()
             pila.apilar(Nodo(lista[0],nodo_izq,nodo_der))
@@ -12,7 +19,7 @@ def convertir(lista, pila):
         return convertir(lista[1:],pila)
             
 
-def evaluar(arbol):
+def evaluar(arbol):    
     if arbol.valor == "+":
         return evaluar(arbol.izq) + evaluar(arbol.der)
     if arbol.valor == "-":
@@ -20,37 +27,37 @@ def evaluar(arbol):
     if arbol.valor == "/":
         return evaluar(arbol.izq) / evaluar(arbol.der)
     if arbol.valor == "*":
-        return evaluar(arbol.izq) * evaluar(arbol.der)
+        return evaluar(arbol.izq) * evaluar(arbol.der)    
+    if arbol.valor == "=":        
+        return updateDict(arbol.der, evaluar(arbol.izq))
+    if arbol.valor.isalpha():
+        return dictionary[arbol.valor]
     return int(arbol.valor)
-
 
 entrada = open("expresiones.in","r")
 
-exp = entrada.readline()
-
-str1 = ''.join(exp)
-
-#exp3 = str1.split("\n")
-print str1
-#for i in exp3:
-#    print exp3[i]
-    
-
-entrada.close()
-exp2 = str1.split(" ")
-
-
-
+exp = entrada.readlines()
 
 pila = Pila()
 
-convertir(exp2, pila)
+for i in range(0, len(exp)):        
+    pp = exp[i]
+    str1 = ''.join(pp.strip('\n'))        
+    exp2 = str1.split(" ")   
+    convertir(exp2, pila)
+    
+
+entrada.close()
+
 entrada = open("expresiones.out","w")
 
-res = evaluar(pila.desapilar())
-print res
-str(res)
-entrada.write(str(res))
+while pila.es_vacia() != True:    
+    res = evaluar(pila.desapilar())
+    str(res)
+    entrada.write(str(res))
+    if res!= None:
+        print res
+
 
 entrada.close()
 """3 5 *
